c97eabd1a2ef66614dfb58fbdcd57a69b2a9f57b
Khaled <kmesbah@accengage.com>
2020-08-08 00:01:43 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+const Index = () => {
+  return (
+    <section className="section">
+      <h1 className="title">KeyServices</h1>
+      <p className="subtitle">
+        Hosting guests will no longer require any effort.
+      </p>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Input" ></input>
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <div className="buttons">
+            <a className="button is-primary">Primary</a>
+            <a className="button is-link">Link</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Index;
