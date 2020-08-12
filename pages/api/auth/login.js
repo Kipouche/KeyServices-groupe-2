08e6211410f09f1443fd7eef89bf72e2c57ba334
Julian Schumacker <schumacker.julian@gmail.com>
2020-08-12 16:49:49 +0200

    Update Login

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -18,7 +18,7 @@ async function passCompare(password, hashPassword) {
 export default async (req, res) => {
   // ajouter le jsonwebtoken sur les cookies de la réponse
   // vérifier si le user est active ou pas
-  const { email, password } = req.body;
+  const {email, password } = req.body;
 
   if (req.method === 'POST') {
     if (!email || !password) {
@@ -35,6 +35,12 @@ export default async (req, res) => {
       const user = results[0];
       try {
         await passCompare(password, user.password);
+        const claims = {sub: user.id, myPersonEmail: user.email}; /// ??? failed
+        jwt.sign(claims, '81248b68-d188-47bb-aca3-1201ba51897c');
+        res.json({authToken: jwt});
+        // CHekc JWT token before authorization (secure, httpOnly SameSite to yourr cookies)
+        // Si match request immediatly
+        // Sinon error 401
         return res.status(200).json({ message: 'Connected' });
       } catch (error) {
         return res.status(401).json({ message: error.message });
