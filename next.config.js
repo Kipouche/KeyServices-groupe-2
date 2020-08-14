c27812e5757f859340674b7bd593c0d5939559fa
Khaled <kmesbah@accengage.com>
2020-08-14 17:45:57 +0200

    modified display type on PWA, add splash screen image, fix config file for PWA

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
 const withSass = require('@zeit/next-sass');
 const withPWA = require('next-pwa');
 
-module.exports = withPWA(
-  withSass({
+if (process.env.NODE_ENV === 'development') {
+  module.exports = withSass({
     env: {
       SECRET: '84f929ab-2819-4f37-aa1f-98e4d7cae451',
       SECRET_SHA: '0e259e21-dce4-4d39-b37d-d183deee828e'
     },
     pwa: {
       dest: 'public'
-  }
-
-  })
-);
+    }
+  });
+} else {
+  module.exports = withPWA(
+    withSass({
+      env: {
+        SECRET: '84f929ab-2819-4f37-aa1f-98e4d7cae451',
+        SECRET_SHA: '0e259e21-dce4-4d39-b37d-d183deee828e'
+      },
+      pwa: {
+        dest: 'public'
+      }
+    })
+  );
+}
