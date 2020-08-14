afbf94f7b2c86f46226db51c1757770bf7a14317
Khaled <kmesbah@accengage.com>
2020-08-14 17:13:12 +0200

    add icons and meta tags for PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const withSass = require('@zeit/next-sass');
+const withPWA = require('next-pwa');
 
-module.exports = withSass({
-  env: {
-    SECRET: '84f929ab-2819-4f37-aa1f-98e4d7cae451',
-    SECRET_SHA: '0e259e21-dce4-4d39-b37d-d183deee828e'
+module.exports = withPWA(
+  withSass({
+    env: {
+      SECRET: '84f929ab-2819-4f37-aa1f-98e4d7cae451',
+      SECRET_SHA: '0e259e21-dce4-4d39-b37d-d183deee828e'
+    },
+    pwa: {
+      dest: 'public'
   }
-});
+
+  })
+);
