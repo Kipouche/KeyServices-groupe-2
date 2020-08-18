ea67acd0e689e7d4e6b6f11813b2a39f6c8b0dbb
Julian Schumacker <schumacker.julian@gmail.com>
2020-08-18 19:07:19 +0200

    Update Profile

--- pages/api/profile/[profileId].js
+++ pages/api/profile/[profileId].js
@@ -1,6 +1,7 @@
 import authentification from '../../../lib/authentification';
 import User from '../../../lib/user';
 import convertTimeGMT2 from '../../../lib/convertTimeGMT2';
+import InputValidation from '../../../lib/inputValidation';
 
 export default authentification(async (req, res) => {
   const { profileId } = req.query;
@@ -19,8 +20,39 @@ export default authentification(async (req, res) => {
     return res.status(200).json({ profile: true });
   }
   if (req.method === 'PUT') {
-      console.log('ok upd');
-    return res.status(200).json({ profile: true });
+    const { firstname, lastname, phonenumber, dateofbirth } = req.body;
+    if (!firstname || !lastname || !phonenumber || !dateofbirth) {
+      return res.status(401).json({ message: 'A field is missing' });
+    }
+    if (
+      !InputValidation.verifyName(lastname) ||
+      !InputValidation.verifyName(firstname)
+    ) {
+      console.log('Invalid firstname or lastname');
+      return res.status(401).json({ message: 'Invalid firstname or lastname' });
+    }
+    if (!InputValidation.verifyPhonenumber(phonenumber)) {
+      console.log('Invalid phone number');
+      return res.status(401).json({ message: 'Invalid phone number' });
+    }
+    if (InputValidation.isLess18ThanYears(dateofbirth)) {
+      console.log('User musts be at least 18 years old');
+      return res
+        .status(401)
+        .json({ message: 'User musts be at least 18 years old' });
+    }
+    try {
+      await User.updateProfile(
+        profileId,
+        firstname,
+        lastname,
+        phonenumber,
+        dateofbirth
+      );
+      return res.status(200).json({ message: 'Successfull updating profile' });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
   }
   return res.status(405).json({ message: `${req.method} doesn't exists` });
 });
