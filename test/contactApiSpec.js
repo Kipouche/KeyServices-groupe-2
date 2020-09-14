4787bb8a20471f9703d63070b36b9df9e5bf6660
JeremyLichtmann <49403215+JeremyLichtmann@users.noreply.github.com>
2020-09-14 17:04:08 +0200

    test API login + register, modif confirm, ajout fichier rapport junit

--- test/contactApiSpec.js
+++ test/contactApiSpec.js
@@ -13,97 +13,107 @@ const { expect } = chai;
 const host = 'http://localhost:5000';
 
 describe('Test API contact', function () {
-  it('Test wrong method, should return status 400 with message : Only method POST exists', function (done) {
-    chai
-      .request(host)
-      .patch('/api/contact')
-      .type('form')
-      .send({
-        email: 'test@mail.com',
-        name: 'nameTest',
-        subject: 'subjectTest',
-        message: 'messageTest'
-      })
-      .end(function (err, res) {
-        expect(res).to.have.status(400);
-        expect(res)
-          .to.have.property('text')
-          .to.equal('{"message":"Only method POST exists"}');
-        done();
-      });
+  describe('# Test wrong method', function () {
+    it('Should return status 400 with message : Only method POST exists', function (done) {
+      chai
+        .request(host)
+        .patch('/api/contact')
+        .type('form')
+        .send({
+          email: 'test@mail.com',
+          name: 'nameTest',
+          subject: 'subjectTest',
+          message: 'messageTest'
+        })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res)
+            .to.have.property('text')
+            .to.equal('{"message":"Only method POST exists"}');
+          done();
+        });
+    });
   });
-  it('Test without all fields, should return status 400 with message : A field is missing', function (done) {
-    chai
-      .request(host)
-      .post('/api/contact')
-      .type('form')
-      .send({
-        email: 'test@mail.com',
-        name: 'nameTest',
-        subject: '',
-        message: 'messageTest'
-      })
-      .end(function (err, res) {
-        expect(res).to.have.status(400);
-        expect(res)
-          .to.have.property('text')
-          .to.equal('{"message":"A field is missing"}');
-        done();
-      });
+  describe('# Test without all fields', function () {
+    it('Should return status 400 with message : A field is missing', function (done) {
+      chai
+        .request(host)
+        .post('/api/contact')
+        .type('form')
+        .send({
+          email: 'test@mail.com',
+          name: 'nameTest',
+          subject: '',
+          message: 'messageTest'
+        })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res)
+            .to.have.property('text')
+            .to.equal('{"message":"A field is missing"}');
+          done();
+        });
+    });
   });
-  it('Test with an invalid email length, should return status 400 with message : Invalid email', function (done) {
-    chai
-      .request(host)
-      .post('/api/contact')
-      .type('form')
-      .send({
-        email: 'te@l.com',
-        name: 'nameTest',
-        subject: 'subjectTest',
-        message: 'messageTest'
-      })
-      .end(function (err, res) {
-        expect(res).to.have.status(400);
-        expect(res)
-          .to.have.property('text')
-          .to.equal('{"message":"Invalid email"}');
-        done();
-      });
+  describe('# Test with an invalid email length', function () {
+    it('Should return status 400 with message : Invalid email', function (done) {
+      chai
+        .request(host)
+        .post('/api/contact')
+        .type('form')
+        .send({
+          email: 'te@l.com',
+          name: 'nameTest',
+          subject: 'subjectTest',
+          message: 'messageTest'
+        })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res)
+            .to.have.property('text')
+            .to.equal('{"message":"Invalid email"}');
+          done();
+        });
+    });
   });
-  it('Test with an invalid name, should return status 400 with message : Invalid name', function (done) {
-    chai
-      .request(host)
-      .post('/api/contact')
-      .type('form')
-      .send({
-        email: 'mail@mail.com',
-        name: 'name8Test',
-        subject: 'subjectTest',
-        message: 'messageTest'
-      })
-      .end(function (err, res) {
-        expect(res).to.have.status(400);
-        expect(res)
-          .to.have.property('text')
-          .to.equal('{"message":"Invalid name"}');
-        done();
-      });
+  describe('# Test with an invalid name', function () {
+    it('Should return status 400 with message : Invalid name', function (done) {
+      chai
+        .request(host)
+        .post('/api/contact')
+        .type('form')
+        .send({
+          email: 'mail@mail.com',
+          name: 'name8Test',
+          subject: 'subjectTest',
+          message: 'messageTest'
+        })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res)
+            .to.have.property('text')
+            .to.equal('{"message":"Invalid name"}');
+          done();
+        });
+    });
   });
-  it('Should be a success, return status 200 with success : ok', function (done) {
-    chai
-      .request(host)
-      .post('/api/contact')
-      .type('form')
-      .send({
-        email: 'mail@mail.com',
-        name: 'nameTest',
-        subject: 'subjectTest',
-        message: 'messageTest'
-      })
-      .end(function (err, res) {
-        expect(res).to.have.status(200);
-        expect(res).to.have.property('text').to.equal('{"success":"ok"}');
-        done();
-      });
+  describe('# Success', function () {
+    it('Should be a success, return status 200 with success : ok', function (done) {
+      chai
+        .request(host)
+        .post('/api/contact')
+        .type('form')
+        .send({
+          email: 'mail@mail.com',
+          name: 'nameTest',
+          subject: 'subjectTest',
+          message: 'messageTest'
+        })
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          expect(res).to.have.property('text').to.equal('{"success":"ok"}');
+          done();
+        });
+    });
   });
 });
---
+++ test/registerApiSpec.js
@@ -0,0 +1,247 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+/* const host = process.env.NODE_ENV !== 'development'
+            ? 'https://keyserviceshost.vercel.app'
+            : 'http://localhost:5000'; */
+const host = 'http://localhost:5000';
+
+describe('Test API register', function () {
+    describe('# Test wrong method', function(){
+        it('Should return status 400 with message : Only method POST exists', function (done) {
+            chai
+              .request(host)
+              .patch('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(400);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Only method POST exists"}');
+                done();
+              });
+          });
+    });
+    describe('# Test without all fields', function(){
+        it('Should return status 401 with message : A field is missing', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"A field is missing"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid email length', function(){
+        it('Should return status 401 with message : Invalid email', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'l@g.cm',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Invalid email"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid email format', function(){
+        it('Should return status 401 with message : Invalid email', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Invalid email"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid password length', function(){
+        it('Should return status 401 with message : Password is too short', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '1234567',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Password is too short"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid firstname', function(){
+        it('Should return status 401 with message : Invalid firstname or lastname', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstname@Te2st',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Invalid firstname or lastname"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid laststname', function(){
+        it('Should return status 401 with message : Invalid firstname or lastname', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastname2Test',
+                phonenumber: '0610101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Invalid firstname or lastname"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an invalid phonenumber', function(){
+        it('Should return status 401 with message : Invalid phone number', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '010101010',
+                dateofbirth: '1990-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"Invalid phone number"}');
+                done();
+              });
+          });
+    });
+    describe('# Test with an age < 18', function(){
+        it('Should return status 401 with message : User musts be at least 18 years old', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: 'test@mail.com',
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '2005-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(401);
+                expect(res)
+                  .to.have.property('text')
+                  .to.equal('{"message":"User musts be at least 18 years old"}');
+                done();
+              });
+          });
+    });
+    describe('# Test without any problem', function(){
+        it('Should return status 200 with success', function (done) {
+            chai
+              .request(host)
+              .post('/api/auth/register')
+              .type('form')
+              .send({
+                email: `${
+                  Math.random().toString(36).substring(2, 15) +
+                  Math.random().toString(36).substring(2, 15)
+                }@mail.com`,
+                password: '123456789',
+                firstname: 'firstnameTest',
+                lastname: 'lastnameTest',
+                phonenumber: '0610101010',
+                dateofbirth: '1995-11-18'
+              })
+              .end(function (err, res) {
+                expect(res).to.have.status(200);
+                expect(res).to.have.property('text').to.include('success');
+                done();
+              });
+          });
+    });
+});
